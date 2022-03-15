0b7c8635b6add10c36a05de260bd198f383417d1
棋纬 <wenyong.hwy@alibaba-inc.com>
2022-03-15 19:28:35 +0800

    update import

--- RibutIOS/AliRibutSDK/AliRibutSDK.h
+++ RibutIOS/AliRibutSDK/AliRibutSDK.h
@@ -37,7 +37,7 @@ FOUNDATION_EXPORT const unsigned char AliRibutSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AliRibutSDK/PublicHeader.h>
 
-#import <AliRibutSDK/AliRibutProtocol.h>
-#import <AliRibutSDK/AliRibutManager.h>
+#import "AliRibutProtocol.h"
+#import "AliRibutManager.h"
 
 
